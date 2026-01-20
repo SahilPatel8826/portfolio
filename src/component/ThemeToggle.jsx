10dc8ee886edfa64cfd4c641fb49dfe336bdc449
SahilPatel8826 <164241614+SahilPatel8826@users.noreply.github.com>
2026-01-21 03:02:08 +0530

    fix: remove mongoose import from ThemeToggle

--- src/component/ThemeToggle.jsx
+++ src/component/ThemeToggle.jsx
@@ -4,7 +4,7 @@ import {Sun,Moon} from "lucide-react";
 import { cn } from "@/lib/utils";
 
 export const ThemeToggle = () => {
-  const  [isDarkMode, setIsDarkMode] = useState(false);
+  const  [isDarkMode, setIsDarkMode] = useState(true);
 
 
   useEffect(()=>{
